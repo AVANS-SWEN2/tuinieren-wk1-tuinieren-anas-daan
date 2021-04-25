aa960880615cea1dee601c8e3abe09c0dda330ef
Anas073 <82931055+Anas073@users.noreply.github.com>
2021-04-25 17:04:01 +0200

    TuinDomotica.java update

--- TuinDomotica.java
+++ TuinDomotica.java
@@ -1,17 +1,24 @@
 public class TuinDomotica {
-    
     private Boolean daglicht;
     private Boolean regen;
     private Schakelaar slimmeschakelaar;
     private int tijdstip;
-    tijdstip = 21;
     
+    tijdstip = 21;
 
     public TuinDomotica() {
         super();
         slimmeschakelaar = Schakelaar.AUTOMATISCH;
+        
     }
     
+    public Aansturing() {
+
+        // Als de schakelaar op AAN staat zal de verlichting worden aangezet samen met de sproeier
+        // Als de schakelaar op UIT staat zal de verlichting worden uitgezet samen met de sproeier
+        // Als de schakelaar op AUTOMATISCH staat zal de verlichting aan gaan tussen 20:00 en 05:00, en de sproeier tussen 20:00 en 05:00 als het niet regent.
+    }
+        
     public Boolean getDaglicht() {
         return daglicht;
     }
